3959e18b0cdff324616b596b7112ef0c841c1d69
Brad <brad@brad.tw>
2016-09-22 19:51:32 +0800

    v5

--- app/src/main/java/brad/tw/guessnumber/MainActivity.java
+++ app/src/main/java/brad/tw/guessnumber/MainActivity.java
@@ -2,7 +2,10 @@ package brad.tw.guessnumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
     private String answer;
@@ -12,16 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        answer = createAnswer();
+        answer = createAnswer(3);
+        //Log.d("brad", answer);
+
     }
 
     public void doGuess(View v){
-
     }
 
     // create a answer
-    private String createAnswer(){
-        return "417";
+    private String createAnswer(int n){
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size()<n){
+            set.add((int)(Math.random()*10));
+        }
+        StringBuffer sb = new StringBuffer();
+        for (Integer i : set){
+            sb.append(i);
+        }
+        return sb.toString();
     }
     // check ?A?B
     private String checkAB(){
