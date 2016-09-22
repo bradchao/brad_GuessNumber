b4adef6ec70cbdd55279591fe74f4c35f0a4efbb
Brad <brad@brad.tw>
2016-09-22 20:10:31 +0800

    v6

--- app/src/main/java/brad/tw/guessnumber/MainActivity.java
+++ app/src/main/java/brad/tw/guessnumber/MainActivity.java
@@ -4,16 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
     private String answer;
+    private TextView info;
+    private EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        info = (TextView)findViewById(R.id.textInfo);
+        input = (EditText)findViewById(R.id.editGuess);
 
         answer = createAnswer(3);
         //Log.d("brad", answer);
@@ -21,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doGuess(View v){
+        String guess = input.getText().toString();
+        String result = checkAB(answer, guess);
+        info.append(guess + ":" + result + "\n");
+        input.setText("");
     }
 
     // create a answer
@@ -36,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         return sb.toString();
     }
     // check ?A?B
-    private String checkAB(){
+    private String checkAB(String a, String g){
         return "1A2B";
     }
 
